262baffb3c286c872244a7337d8f95d8a512b2fd
Tileryr <tileryrchicken@gmail.com>
2025-05-24 14:24:17 -0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Panel from "./Panel"
+
+function App() {
+  return (
+    <>
+      <div className='bg-zinc-900 h-screen w-screen flex'>
+        <Panel></Panel>
+        <Panel></Panel>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Panel.tsx
@@ -0,0 +1,11 @@
+function Panel() {
+    return (
+    <div className="bg-zinc-800 m-3 w-full rounded flex items-center justify-center">
+        <div>
+            <img src="https://picsum.photos/400" alt="GORRILLA"></img>
+        </div>
+    </div>
+    )
+}
+
+export default Panel
