7e4e64ca25c6948acd9cfb5246778224341c56f7
Tileryr <tileryrchicken@gmail.com>
2025-05-26 20:29:54 -0400

    get wikipedia image

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import Panel from "./Panel"
 
 function App() {
   return (
-    <>
+    <> 
       <div className='bg-zinc-900 h-screen w-screen flex'>
         <Panel></Panel>
+        <div className="grid grid-rows-[1fr_auto_1fr]">
+          <strong className="text-6xl text-white row-end-3">
+          VS
+          </strong>
+        </div>
         <Panel></Panel>
       </div>
     </>
--- src/Panel.tsx
+++ src/Panel.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from "react"
+
 function Panel() {
+    const [imageSrc, setImageSrc] = useState<string | null>(null)
+    
+    const wikipedia_api_url = "https://en.wikipedia.org/api/rest_v1/page/html/Gorilla"
+
+
+    async function get_wikipedia_info() {
+        const api_response = await fetch(wikipedia_api_url)
+        const page_html_string = await api_response.text()
+        const page = new DOMParser().parseFromString(page_html_string, "text/html")
+        const image = page.querySelector("img")!
+        setImageSrc(image.src)
+        console.log(page)
+    }
+
+    useEffect(() => {
+        get_wikipedia_info()
+    }, [])
+
+    
+   
+
     return (
     <div className="bg-zinc-800 m-3 w-full rounded flex items-center justify-center">
-        <div>
-            <img src="https://picsum.photos/400" alt="GORRILLA"></img>
+        <div className="h-2/3 p-8 bg-zinc-700 rounded">
+            <img src={imageSrc ? imageSrc : undefined} alt="GORRILLA"></img>
+            <p className="text-white text-2xl">
+                <strong>
+                Gorrila
+                </strong>
+            </p>
+            <ul className="text-white text-xl list-disc ml-8">
+                <li>Strength</li>
+                <li>Weight</li>
+                <li>Height</li>
+                <li>Speed</li>
+            </ul>
         </div>
     </div>
     )
